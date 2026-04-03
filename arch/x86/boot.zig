b9c98415a17a4f1faaee72d04e69b2b075ef75f3
M.S <ms@hadronlang.com>
2026-04-03 18:44:18 +0200

    feat: migrate to multiboot2 and request framebuffer mode

--- arch/x86/boot.zig
+++ arch/x86/boot.zig
@@ -1,23 +1,74 @@
-const MAGIC: u32 = 0x1BADB002;
-const FLAGS: u32 = 3;
+const std = @import("std");
 
-const MultibootHeader = extern struct {
-    magic: u32,
-    flags: u32,
-    checksum: u32,
+const InformationRequestTag = extern struct {
+    type: u16 = 1,
+    flags: u16 = 0,
+    size: u32 = @sizeOf(InformationRequestTag),
+    requests: [2]u32 = .{ 1, 8 },
 };
 
-export var multiboot_header: MultibootHeader align(4) linksection(".multiboot") = .{
-    .magic = MAGIC,
-    .flags = FLAGS,
-    .checksum = 0 -% (MAGIC +% FLAGS),
+const ConsoleTag = extern struct {
+    type: u16 = 4,
+    flags: u16 = 1, // optional
+    size: u32 = @sizeOf(ConsoleTag),
+    console_flags: u32 = (1 << 1), // text mode
+    _pad: u32 = 0,
 };
+
+const FramebufferTag = extern struct {
+    type: u16 = 5,
+    flags: u16 = 1, // optional
+    size: u32 = @sizeOf(FramebufferTag),
+    width: u32 = 1024,
+    height: u32 = 768,
+    depth: u32 = 32,
+    _pad: u32 = 0,
+};
+
+const EndTag = extern struct {
+    type: u16 = 0,
+    flags: u16 = 0,
+    size: u32 = 8,
+};
+
+const Multiboot2Header = extern struct {
+    magic: u32 = 0xE85250D6,
+    architecture: u32 = 0, // i386
+    header_length: u32 = @sizeOf(Multiboot2Header),
+    checksum: u32 = 0,
+    info_req: InformationRequestTag = .{},
+    console: ConsoleTag = .{},
+    framebuffer: FramebufferTag = .{},
+    end: EndTag = .{},
+};
+
+fn makeHeader() Multiboot2Header {
+    var h = Multiboot2Header{};
+    h.checksum = 0 -% (h.magic +% h.architecture +% h.header_length);
+    return h;
+}
+
+comptime {
+    std.debug.assert(@sizeOf(InformationRequestTag) % 8 == 0);
+    std.debug.assert(@sizeOf(ConsoleTag) % 8 == 0);
+    std.debug.assert(@sizeOf(FramebufferTag) % 8 == 0);
+    std.debug.assert(@sizeOf(EndTag) % 8 == 0);
+}
+
+export var multiboot2_header: Multiboot2Header align(8) linksection(".multiboot2") = makeHeader();
 
 export var stack: [16384]u8 align(16) linksection(".bss") = undefined;
 
 export fn _start() callconv(.naked) noreturn {
     asm volatile (
         \\movl $stack + 16384, %%esp
+        // Reset EFLAGS
+        \\pushl $0
+        \\popf
+        // Push multiboot information structure pointer
+        \\pushl %%ebx
+        // Push magic value
+        \\pushl %%eax
         \\call kmain
         \\sti
         \\.Lhalt:
---
+++ arch/x86/multiboot2.zig
@@ -0,0 +1,203 @@
+const vbe = @import("vbe.zig");
+
+pub const BOOTLOADER_MAGIC: u32 = 0x36d76289;
+
+pub const Info = extern struct {
+    total_size: u32,
+    reserved: u32,
+};
+
+pub const TagType = enum(u32) {
+    end = 0,
+    cmdline = 1,
+    // boot_loader_name = 2,
+    // module = 3,
+    // basic_meminfo = 4,
+    // bootdev = 5,
+    // mmap = 6,
+    // vbe = 7,
+    framebuffer = 8,
+    // elf_sections = 9,
+    // apm = 10,
+    // efi32_systable = 11,
+    // efi64_systable = 12,
+    // smbios = 13,
+    // acpi_rsdp_v1 = 14,
+    // acpi_rsdp_v2 = 15,
+    // network = 16,
+    // efi_mmap = 17,
+    // efi_boot_not_terminated = 18,
+    // efi32_image_handle = 19,
+    // efi64_image_handle = 20,
+    _,
+};
+
+pub const Tag = extern struct {
+    type: TagType,
+    size: u32,
+};
+
+pub const BootdevTag = extern struct {
+    base: Tag,
+    biosdev: u32,
+    partition: u32,
+    sub_partition: u32,
+};
+
+pub const CmdlineTag = extern struct {
+    base: Tag,
+
+    pub fn string(self: *CmdlineTag) [*:0]u8 {
+        return @ptrFromInt(@intFromPtr(self) + @sizeOf(Tag));
+    }
+};
+
+pub const ModuleTag = extern struct {
+    base: Tag,
+    mod_start: u32,
+    mod_end: u32,
+
+    pub fn string(self: *ModuleTag) [*:0]u8 {
+        return @ptrFromInt(@intFromPtr(self) + @sizeOf(Tag));
+    }
+};
+
+pub const ElfsectionTag = extern struct {
+    base: Tag,
+    num: u16,
+    entsize: u16,
+    shndx: u16,
+    reserved: u16,
+    headers: undefined,
+};
+
+pub const MmapEntry = extern struct {
+    base_addr: u64,
+    length: u64,
+    type: u32,
+    reserved: u32,
+};
+
+pub const MmapTag = extern struct {
+    base: Tag,
+    entry_size: u32,
+    entry_version: u32,
+
+    pub fn entries(self: *MmapTag) [*]MmapEntry {
+        return @ptrFromInt(@intFromPtr(self) + @sizeOf(Tag));
+    }
+};
+
+pub const BootloaderNameTag = extern struct {
+    base: Tag,
+
+    pub fn string(self: *BootloaderNameTag) [*:0]u8 {
+        return @ptrFromInt(@intFromPtr(self) + @sizeOf(Tag));
+    }
+};
+
+pub const ApmTag = extern struct {
+    base: Tag,
+    version: u16,
+    cseg: u16,
+    offset: u32,
+    cseg_16: u16,
+    dseg: u16,
+    flags: u16,
+    cseg_len: u16,
+    cseg_16_len: u16,
+    dseg_len: u16,
+};
+
+pub const VbeTag = extern struct {
+    base: Tag,
+    vbe_mode: u16,
+    vbe_interface_seg: u16,
+    vbe_interface_off: u16,
+    vbe_interface_len: u16,
+    vbe_control_info: vbe.VbeInfoBlock,
+    vbe_mode_info: vbe.VbeModeInfoBlock,
+};
+
+pub const FramebufferPalette = extern struct {
+    red: u8,
+    green: u8,
+    blue: u8,
+};
+
+pub const FramebufferIndexed = extern struct {
+    num_colors: u32,
+
+    pub fn palette(self: *FramebufferIndexed) [*]FramebufferPalette {
+        return @ptrFromInt(@intFromPtr(self) + @sizeOf(FramebufferIndexed));
+    }
+};
+
+pub const FramebufferDirect = extern struct {
+    red_pos: u8,
+    red_mask: u8,
+    green_pos: u8,
+    green_mask: u8,
+    blue_pos: u8,
+    blue_mask: u8,
+};
+
+pub const FramebufferType = enum(u8) {
+    indexed = 0,
+    direct = 1,
+    ega_text = 2,
+};
+
+pub const FramebufferColorInfo = union(FramebufferType) {
+    indexed: FramebufferIndexed,
+    direct: FramebufferDirect,
+};
+
+pub const FramebufferTag = extern struct {
+    base: Tag,
+    addr: u64,
+    pitch: u32,
+    width: u32,
+    height: u32,
+    bpp: u8,
+    type: FramebufferType,
+    reserved: u8,
+
+    pub fn colorInfo(self: *FramebufferTag) FramebufferColorInfo {
+        const addr = @intFromPtr(self) + @sizeOf(FramebufferTag);
+        return switch (self.fb_type) {
+            .indexed => .{ .indexed = @as(*FramebufferIndexed, @ptrFromInt(addr)).* },
+            .direct => .{ .direct = @as(*FramebufferDirect, @ptrFromInt(addr)).* },
+        };
+    }
+};
+
+pub fn parse(info: *Info, handlers: anytype) void {
+    var addr = @intFromPtr(info) + 8;
+    const end = @intFromPtr(info) + info.total_size;
+
+    while (addr < end) {
+        const tag: *Tag = @ptrFromInt(addr);
+
+        switch (tag.type) {
+            .end => break,
+            .cmdline => if (@hasDecl(handlers, "onCmdline"))
+                handlers.onCmdline(@ptrFromInt(addr)),
+            .framebuffer => if (@hasDecl(handlers, "onFramebuffer"))
+                handlers.onFramebuffer(@ptrFromInt(addr)),
+            _ => {}
+        }
+        addr += (tag.size + 7) & ~@as(usize, 7);
+    }
+}
+
+comptime {
+    const assert = @import("std").debug.assert;
+
+    assert(@sizeOf(Tag) == 8);
+    assert(@sizeOf(MmapEntry) == 24);
+    assert(@offsetOf(MmapEntry, "base_addr") == 0);
+    assert(@offsetOf(MmapEntry, "length") == 8);
+    assert(@sizeOf(MmapTag) == 16);
+    assert(@sizeOf(VbeTag) == 784);
+}
